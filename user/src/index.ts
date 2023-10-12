dfed80e14102d086324df78b18b654d5df1b01e2
HervalMata <hervalmataamparo@gmail.com>
2023-10-12 12:49:51 -0300

    feat:art: microservices - add ingress service

--- user/src/index.ts
+++ user/src/index.ts
@@ -5,6 +5,10 @@ app.use(express.json());
 
 const port = 3000;
 
+app.get("/api/users/currentuser", (req, res) => {
+    res.send("hi there!");
+});
+
 app.listen(port, () => {
     console.log(`User server: Listening on port ${port}`);
 });
