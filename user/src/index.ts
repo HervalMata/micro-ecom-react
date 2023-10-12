0604991d3c22dd1a451f7dd59854deca9030f0c7
HervalMata <hervalmataamparo@gmail.com>
2023-10-12 13:15:05 -0300

    feat:art: microservices - separate routes of user service

--- user/src/index.ts
+++ user/src/index.ts
@@ -1,13 +1,18 @@
 import express from "express";
+import {currentUserRouter} from "./routes/current-user";
+import {signinRouter} from "./routes/signin";
+import {signoutRouter} from "./routes/signout";
+import {signupRouter} from "./routes/signup";
 
 const app = express();
 app.use(express.json());
 
 const port = 3000;
 
-app.get("/api/users/currentuser", (req, res) => {
-    res.send("hi there!");
-});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.listen(port, () => {
     console.log(`User server: Listening on port ${port}`);
